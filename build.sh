3e3b149627b1831389195109432c8c6319a0275b
Enock <djsean@Enocks-MacBook-Pro.local>
2025-04-02 09:32:20 +0300

    new

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ echo "Building the project JDA..."
 
 pip install -r requirements.txt
 
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+python3 manage.py makemigrations 
+python3 manage.py migrate 
