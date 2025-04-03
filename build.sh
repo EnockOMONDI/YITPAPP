075e8b28ba4a39658394b03421cdb19ab9f2532f
Enock <djsean@Enocks-MacBook-Pro.local>
2025-04-03 06:35:54 +0300

    prod local workfgjf

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ echo "Building the lgf."
 
 pip install -r requirements.txt
 
-python3 manage.py collectstatic --noinput
+
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
