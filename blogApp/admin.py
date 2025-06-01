ed2c1d6992c166d6d86ff03dcc56e6a5331403d7
Enock <djsean@Enocks-MacBook-Pro.local>
2025-06-01 21:35:59 +0300

    Fix final import issue in blogApp/admin.py - complete case sensitivity fix

--- blogApp/admin.py
+++ blogApp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blogapp.models import Post, Comment, Category, StaticContent
+from blogApp.models import Post, Comment, Category, StaticContent
 from import_export.admin import ImportExportModelAdmin
 
 class StaticContentAdmin(admin.ModelAdmin):
